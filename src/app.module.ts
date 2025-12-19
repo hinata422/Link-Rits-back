1baec655075908ff8b9ba974d661e6868133c0fc
hinata <yangxiangdaotian20@gmail.com>
2025-12-20 05:11:44 +0900

    feature:testでコンフリクト対策をしてこれからtest-recieved-mergeに送り込む

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,38 +1,48 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { UserController } from './controller/user.controller';
-import { UserService } from './service/user.service';
-import { UserRepositoryImpl } from './repository/user/psql/user.repo.impl';
-import { EventController } from './controller/event.controller';
-import { EventService } from './service/event.service';
-import { EventRepositoryImpl } from './repository/event/psql/event.repo.impl';
-import { EventEditedService } from './service/event-edited.service';
-import { EventEditedRepositoryImpl } from './repository/event-edited/psql/event-edited.repo.impl';
-import { EventEditedController } from './controller/event-edited.controller';
-import { PlainTextToMbtiLikeConverter } from './service/plain-text-to-mbti-like-converter.service';
-import { supabaseClient } from './main';
 import { SupabaseClient } from '@supabase/supabase-js';
+
+import { UserController } from './controller/user.controller';
+import { EventController } from './controller/event.controller';
+import { EventEditedController } from './controller/event-edited.controller';
+
+import { UserService } from './service/user.service';
+import { EventService } from './service/event.service';
+import { EventEditedService } from './service/event-edited.service';
+import { PlainTextToMbtiLikeConverter } from './service/plain-text-to-mbti-like-converter.service';
+
+import { UserRepositoryImpl } from './repository/user/psql/user.repo.impl';
+import { EventRepositoryImpl } from './repository/event/psql/event.repo.impl';
+import { EventEditedRepositoryImpl } from './repository/event-edited/psql/event-edited.repo.impl';
+
+import { supabaseClient } from './main';
 import { TYPES } from '../common/Types';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      // 必要なら envFilePath を後で追加してOK
+      // envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
     }),
   ],
-  controllers: [UserController, EventController, EventEditedController],
+  controllers: [
+    UserController,
+    EventController,
+    EventEditedController,
+  ],
   providers: [
     {
       provide: SupabaseClient,
       useValue: supabaseClient,
     },
     {
-      provide: TYPES.EventRepository,
-      useClass: EventRepositoryImpl,
-    },
-    {
       provide: TYPES.UserRepository,
       useClass: UserRepositoryImpl,
+    },
+    {
+      provide: TYPES.EventRepository,
+      useClass: EventRepositoryImpl,
     },
     {
       provide: TYPES.EventEditedRepository,
@@ -43,6 +53,10 @@ import { TYPES } from '../common/Types';
     EventEditedService,
     PlainTextToMbtiLikeConverter,
   ],
-  exports: [TYPES.EventRepository, TYPES.UserRepository, TYPES.EventEditedRepository],
+  exports: [
+    TYPES.UserRepository,
+    TYPES.EventRepository,
+    TYPES.EventEditedRepository,
+  ],
 })
 export class AppModule {}
