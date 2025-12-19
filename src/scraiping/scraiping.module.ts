15797349a9ea4e4a413d8efca86747d5b495ce57
hasseeee <hasegawa.takuya1204@gmail.com>
2025-12-20 05:55:14 +0900

    fix: delete dubbled fold

--- src/scraiping/scraiping.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ScrapingService } from './scraiping.service';
-import { RitsumeikanStrategy } from './strategies/ritsumeikan.strategy';
-
-@Module({
-  providers: [ScrapingService, RitsumeikanStrategy],
-  exports: [ScrapingService],
-})
-export class ScrapingModule {}
